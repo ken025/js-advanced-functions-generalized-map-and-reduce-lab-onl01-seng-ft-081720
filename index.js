3494d4418e7dfd061be7c66ecc8afd152e6cc813
ken025 <kennethmanuel250@gmail.com>
2020-11-23 20:05:13 -0500

    commit tree 8cf7754700970fbb08a8b0ccefef5fb20760feb4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your functions here
+const map = (arr, fnctn) => arr.map(e => fnctn(e))
+
+const reduce = (src, cb, starting) => {
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+    
+    for (; i < src.length; i++) {
+        r = cb(src[i], r)
+    }
+    
+    return r;
+    }
